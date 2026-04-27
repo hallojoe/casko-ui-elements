d704fdb3d8c7deafab76ead00e6aa24a7506d9e1
Casper Korsgaard <casper.korsgaard@outlook.com>
2026-04-27 17:53:27 +0200

    fix: correct paths for circular-sector-layer-svg and circular-sector-svg elements in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,9 @@ export default defineConfig({
       entry: {
         index: resolve(__dirname, 'src/index.ts'),
         'define-all': resolve(__dirname, 'src/define-all.ts'),
-        'elements/circular-sector-layer-svg': resolve(__dirname, 'src/elements/circular-sector-layer-svg.element.ts'),
-        'elements/circular-sector-svg': resolve(__dirname, 'src/elements/circular-sector-svg.element.ts'),
-        'elements/drag-box': resolve(__dirname, 'src/elements/drag-box.element.ts'),
+        'elements/circular-sector-layer-svg': resolve(__dirname, 'src/elements/circular-sector-svg/circular-sector-layer-svg.element.ts'),
+        'elements/circular-sector-svg': resolve(__dirname, 'src/elements/circular-sector-svg/circular-sector-svg.element.ts'),
+        'elements/drag-box': resolve(__dirname, 'src/elements/drag-box/drag-box.element.ts'),
         'elements/drag-scroll': resolve(__dirname, 'src/elements/drag-scroll.element.ts'),
         'elements/number-aware-input': resolve(__dirname, 'src/elements/number-aware-input.element.ts'),
         'elements/selection-box': resolve(__dirname, 'src/elements/selection-box.element.ts'),

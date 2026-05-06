03f6c1c4a00d183a7b3bbc6b1e5fca43bb1c7587
Casper Korsgaard <casper.korsgaard@outlook.com>
2026-05-06 22:10:59 +0200

    Clean up. Removes complex circular things.

--- src/define-all.ts
+++ src/define-all.ts
@@ -1,19 +1,11 @@
 import { CaskoUiAngleInputElement } from './elements/angle-input.element';
 import { CaskoUiAnchorPointInputElement } from './elements/anchor-point-input.element';
 import { CaskoUiBackgroundColorsElement } from './elements/background-colors.element';
-import { CaskoUiCircularDecorationInputElement } from './elements/circular-decoration-input.element';
-import { CaskoUiCircularInputElement } from './elements/circular-input.element';
-import { CaskoUiCircularSectorLayerSvgElement } from './elements/circular-sector-svg/circular-sector-layer-svg.element';
-import { CaskoUiCircularSectorSvgElement } from './elements/circular-sector-svg/circular-sector-svg.element';
-import { CaskoUiCircularTextInputElement } from './elements/circular-text-input.element';
 import { CaskoUiDragBoxElement } from './elements/drag-box/drag-box.element';
 import { CaskoUiDragScrollElement } from './elements/drag-scroll.element';
-import { CaskoUiFillInputElement } from './elements/fill-input.element';
 import { CaskoUiNumberAwareInputElement } from './elements/number-aware-input.element';
 import { CaskoUiRangeItemElement, CaskoUiRangeThingElement } from './elements/range-thing.element';
 import { CaskoUiSelectionBoxElement } from './elements/selection-box.element';
-import { CaskoUiStrokeInputElement } from './elements/stroke-input.element';
-import { CaskoUiTextInputElement } from './elements/text-input.element';
 import { CaskoUiTokenAwareInputElement } from './elements/token-aware-input.element';
 import { CaskoUiTransformBoxElement } from './elements/transform-box.element';
 
@@ -24,20 +16,12 @@ export function defineCaskoUiElements(): void {
   void CaskoUiAngleInputElement;
   void CaskoUiAnchorPointInputElement;
   void CaskoUiBackgroundColorsElement;
-  void CaskoUiCircularDecorationInputElement;
-  void CaskoUiCircularInputElement;
-  void CaskoUiCircularSectorLayerSvgElement;
-  void CaskoUiCircularSectorSvgElement;
-  void CaskoUiCircularTextInputElement;
   void CaskoUiDragBoxElement;
   void CaskoUiDragScrollElement;
-  void CaskoUiFillInputElement;
   void CaskoUiNumberAwareInputElement;
   void CaskoUiRangeItemElement;
   void CaskoUiRangeThingElement;
   void CaskoUiSelectionBoxElement;
-  void CaskoUiStrokeInputElement;
-  void CaskoUiTextInputElement;
   void CaskoUiTokenAwareInputElement;
   void CaskoUiTransformBoxElement;
 }
--- src/index.ts
+++ src/index.ts
@@ -27,89 +27,6 @@ export {
   type DragBoxChangeDetail,
   type DragBoxChangeSource,
 } from './elements/drag-box/drag-box.element';
-export {
-  CaskoUiFillInputElement,
-  type FillInputChangeDetail,
-  type FillInputChangedProperty,
-  type FillInputChangeSource,
-  type FillInputRule,
-  type FillInputValue,
-} from './elements/fill-input.element';
-export {
-  CaskoUiCircularSectorLayerSvgElement,
-} from './elements/circular-sector-svg/circular-sector-layer-svg.element';
-export {
-  CaskoUiCircularDecorationInputElement,
-  type CircularDecorationInputChangeDetail,
-  type CircularDecorationInputChangedProperty,
-  type CircularDecorationInputChangeSource,
-  type CircularDecorationInputCommitDetail,
-  type CircularDecorationInputCommitSource,
-  type CircularDecorationInputSector,
-  type CircularDecorationInputSectorDecoration,
-  type CircularDecorationInputSelectionChangeDetail,
-  type CircularDecorationInputSelectionSource,
-  type CircularDecorationInputValue,
-  type CircularDecorationInputValueHandleAnchor,
-} from './elements/circular-decoration-input.element';
-export {
-  CaskoUiCircularInputElement,
-  type CircularInputChangeDetail,
-  type CircularInputChangeSource,
-  type CircularInputField,
-  type CircularInputSector,
-} from './elements/circular-input.element';
-export {
-  CaskoUiCircularTextInputElement,
-  type CircularTextInputChangeDetail,
-  type CircularTextInputChangedProperty,
-  type CircularTextInputChangeSource,
-  type CircularTextInputCommitDetail,
-  type CircularTextInputCommitSource,
-  type CircularTextInputMode,
-  type CircularTextInputSector,
-  type CircularTextInputSelectionChangeDetail,
-  type CircularTextInputSelectionSource,
-  type CircularTextInputValue,
-  type CircularTextInputValueItem,
-} from './elements/circular-text-input.element';
-export {
-  CaskoUiCircularSectorSvgElement,
-  renderCircularSectorLayer,
-  renderCircularSectorSvg,
-  type CircularSectorSvgErrorMode,
-} from './elements/circular-sector-svg/circular-sector-svg.element';
-export {
-  buildCircularSectorRenderedLayer,
-  buildCircularSectorRenderedSvg,
-  buildCircularSectorTextItems,
-  DEFAULT_CIRCULAR_SECTOR_GLOBAL_SETTINGS,
-  DEFAULT_CIRCULAR_SECTOR_SVG_SETTINGS,
-  parseCircularSectorNumbers,
-  type CircularSectorAlignment,
-  type CircularSectorColorDefinition,
-  type CircularSectorColorPaletteDefinition,
-  type CircularSectorGenerationSettings,
-  type CircularSectorLayerDecoration,
-  type CircularSectorLayerDefinition,
-  type CircularSectorLayerOverrides,
-  type CircularSectorLayerTextOverrides,
-  type CircularSectorPathMode,
-  type CircularSectorPathTextItem,
-  type CircularSectorPositionedTextItem,
-  type CircularSectorPreviewItem,
-  type CircularSectorPreviewTextItems,
-  type CircularSectorRectangularDirection,
-  type CircularSectorRectangularLayout,
-  type CircularSectorRectangularMode,
-  type CircularSectorRenderedLayer,
-  type CircularSectorRenderedSvg,
-  type CircularSectorSvgConfig,
-  type CircularSectorSvgSettings,
-  type CircularSectorTextAnchor,
-  type CircularSectorTextPlacementStrategy,
-  type CircularSectorValueItem,
-} from './elements/circular-sector-svg/circular-sector-svg.model';
 export { CaskoUiDragScrollElement } from './elements/drag-scroll.element';
 export {
   CaskoUiNumberAwareInputElement,
@@ -150,28 +67,6 @@ export {
   type SelectionBoxOrderDirection,
   type SelectionBoxOrderSource,
 } from './elements/selection-box.element';
-export {
-  CaskoUiStrokeInputElement,
-  type StrokeInputChangeDetail,
-  type StrokeInputChangedProperty,
-  type StrokeInputChangeSource,
-  type StrokeInputLinecap,
-  type StrokeInputLinejoin,
-  type StrokeInputType,
-  type StrokeInputValue,
-  type StrokeInputVectorEffect,
-} from './elements/stroke-input.element';
-export {
-  CaskoUiTextInputElement,
-  type TextInputAlignmentBaseline,
-  type TextInputAnchor,
-  type TextInputChangeDetail,
-  type TextInputChangedProperty,
-  type TextInputChangeSource,
-  type TextInputDominantBaseline,
-  type TextInputLengthAdjust,
-  type TextInputValue,
-} from './elements/text-input.element';
 export {
   CaskoUiTransformBoxElement,
   type TransformBoxChangeDetail,
